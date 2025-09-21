53997c29315d84fd9e5655948b27d2acb55def09
duyanh <duyanh011286@gmail.com>
2025-09-21 22:38:17 +0700

    update

--- tests-examples/examples-04/06.stringutils.js
+++ tests-examples/examples-04/06.stringutils.js
@@ -1,9 +1,27 @@
-let className = "     Playwright18 ";
+
+/*let className = "  Playwright K18   ";
 console.log(className.trim());
 console.log(className);
 console.log(className.toLowerCase().trim());
 console.log(className.toUpperCase().trim());
 
+//include(): true,false
+console.log(className.toLowerCase().trim().includes("k18"));
+//replace()
+console.log(className.replace("Playwright", "Selenium"));
+//Split()
+let emails = "duyanh@gmail.com, toan.le@gmail.com, phuong.thao@gmail.com";
+const arrEmails = emails.split("@");
+console.log(arrEmails);
+//substring();
+console.log(className.trim().substring(0, 10));
+console.log(className.trim().substring(6));
+//indexof()
+console.log(className.trim().indexOf("z"));
+*/
 
-//include
-
+let className2 = " Regular Expression Exercise";
+//Split()
+console.log(className2.split(" "));
+//indexof()
+console.log(className2.trim().indexOf("Ex"));
